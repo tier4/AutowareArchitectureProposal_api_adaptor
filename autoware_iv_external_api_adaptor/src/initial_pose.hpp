dcdf30816d7f586ced459a21133db146f99aba09
Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>
2025-01-21 14:13:24 +0900

    Revert "feat: rename autoware_component_interface_specs to autoware_component_interface_specs_universe (#130)"
    This reverts commit bd536c9e9bdf71c0c275d5f4e0200e1b4ac653d7.

--- autoware_iv_external_api_adaptor/src/initial_pose.hpp
+++ autoware_iv_external_api_adaptor/src/initial_pose.hpp
@@ -15,7 +15,7 @@
 #ifndef INITIAL_POSE_HPP_
 #define INITIAL_POSE_HPP_
 
-#include <autoware/component_interface_specs_universe/localization.hpp>
+#include <autoware/component_interface_specs/localization.hpp>
 #include <autoware/component_interface_utils/rclcpp.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tier4_api_utils/tier4_api_utils.hpp>
@@ -40,7 +40,7 @@ private:
   tier4_api_utils::Service<InitializePose>::SharedPtr srv_set_initialize_pose_;
   tier4_api_utils::Service<InitializePoseAuto>::SharedPtr srv_set_initialize_pose_auto_;
   autoware::component_interface_utils::Client<
-    autoware::component_interface_specs_universe::localization::Initialize>::SharedPtr
+    autoware::component_interface_specs::localization::Initialize>::SharedPtr
     cli_localization_initialize_;
 
   // ros callback
